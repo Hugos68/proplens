2950124037bc4156e51f1f7ce736107c147fbffb
hugos68 <hugokorteapple@gmail.com>
2024-11-03 16:07:36 +0100

    Fix formatting and stuff

--- test/fixtures.test.ts
+++ test/fixtures.test.ts
@@ -3,19 +3,32 @@
 import { describe, expect, test } from "vitest";
 import { getPropsFromSvelte } from "../src";
 
-const fixtures = import.meta.glob("./fixtures/*.svelte", {
+const fixtures = import.meta.glob("./fixtures/**/*", {
 	query: "?raw",
 	import: "default",
 });
 
+const frameworkPropsGetterMap = new Map([["svelte", getPropsFromSvelte]]);
+
 describe("fixtures", () => {
 	for (const [path, getCode] of Object.entries(fixtures)) {
 		const name = path.split("/").at(-1);
-		test(name ?? path, async () => {
-			const code = await getCode();
-			const actual = getPropsFromSvelte(String(code));
+		if (!name) {
+			continue;
+		}
+		const extension = name.split(".").at(-1);
+		if (!extension) {
+			continue;
+		}
+		const propsGetter = frameworkPropsGetterMap.get(extension);
+		if (!propsGetter) {
+			continue;
+		}
+		test(name, async () => {
+			const code = String(await getCode());
+			const actual = propsGetter(code);
 			const expected = await import(
-				`${path.replace("fixtures", "results").replace(".svelte", ".json")}`
+				path.replace("fixtures", "results").replace(".svelte", ".json")
 			).then((module) => module.default);
 			expect(actual).toStrictEqual(expected);
 		});
